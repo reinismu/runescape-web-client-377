e170aeec23b3d905ad1e34e8e4041506f2359bf7
Detuks <raimen12@gmail.com>
2019-07-29 23:34:46 +0300

    Changed cache to map

--- osrs/collection/Cache.ts
+++ osrs/collection/Cache.ts
@@ -1,61 +1,21 @@
 import { CacheableNode } from "./CacheableNode";
-import { HashTable } from "./HashTable";
-import { Queue } from "./Queue";
 
 export class Cache {
-    public misses: number;
-
-    public hits: number;
-
-    public capacity: number;
-
-    public remaining: number;
-
-    public hashTable: HashTable;
-
-    public queue: Queue = new Queue();
+    map: Map<number, CacheableNode> = new Map();
 
     public constructor(size: number) {
-        if (this.misses === undefined) { this.misses = 0; }
-        if (this.hits === undefined) { this.hits = 0; }
-        if (this.capacity === undefined) { this.capacity = 0; }
-        if (this.remaining === undefined) { this.remaining = 0; }
-        if (this.hashTable === undefined) { this.hashTable = null; }
-        this.capacity = size;
-        this.remaining = size;
-        this.hashTable = new HashTable(1024);
+
     }
 
     public get(id: number): CacheableNode {
-        const cacheablenode: CacheableNode = this.hashTable.get(id) as CacheableNode;
-        if (cacheablenode != null) {
-            this.queue.push(cacheablenode);
-        }
-        return cacheablenode;
+        return this.map[id];
     }
 
     public put(cacheableNode: CacheableNode, id: number) {
-        if (this.remaining === 0) {
-            const oldestNode: CacheableNode = this.queue.pop();
-            oldestNode.remove();
-            oldestNode.clear();
-        } else {
-            this.remaining--;
-        }
-        this.hashTable.put(cacheableNode, id);
-        this.queue.push(cacheableNode);
+        this.map.set(cacheableNode.id, cacheableNode);
     }
 
     public removeAll() {
-        do {{
-            const node: CacheableNode = this.queue.pop();
-            if (node != null) {
-                node.remove();
-                node.clear();
-            } else {
-                this.remaining = this.capacity;
-                return;
-            }
-        }} while ((true));
+        this.map = new Map();
     }
 }
